44e36e582b4478fa34ae28215df04a42e6edeae9
Pavel <pavelatavin@yandex.ru>
2022-12-16 13:54:56 +0500

    fill readme and create correct prog

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array ={"ab","gf","bg","abc","abcd","abcde","del","hz","123"};
+
+
+string j= string.Empty;
+int n =0;
+    for (int i=0;i<array.Length ; i++)
+        {
+             j=array[i];
+                
+            if (j.Length<=3)
+                { 
+                    n++;
+                }
+        }
+string [] temp = new string [n];
+int k=0;
+for (int i=0;i<array.Length ; i++)
+        {
+             j=array[i];
+                
+            if (j.Length<=3)
+                {
+                    temp[k]=j;
+                    k++;
+                }
+        }
+        for (int i=0; i<temp.Length;i++)
+        {
+            Console.WriteLine (temp[i]);
+        }
